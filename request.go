df860744d68f0b5424c13712abc8e9b73715b0f5
kiyor <cai@kiyor.com>
2017-01-16 20:03:24 +0000

    change proxy buffer size to 16k

--- request.go
+++ request.go
@@ -353,7 +353,22 @@ func sendReply(w io.Writer, resp uint8, addr *AddrSpec) error {
 // proxy is used to suffle data from src to destination, and sends errors
 // down a dedicated channel
 func proxy(dst io.Writer, src io.Reader, errCh chan error) {
-	_, err := io.Copy(dst, src)
+	// 	_, err := io.Copy(dst, src)
+	// 	if tcpConn, ok := dst.(closeWriter); ok {
+	// 		tcpConn.CloseWrite()
+	// 	}
+	// 	errCh <- err
+
+	var err error
+	for {
+		_, err = io.CopyN(dst, src, 16*1024)
+		if err != nil {
+			break
+		}
+	}
+	if err == io.EOF {
+		err = nil
+	}
 	if tcpConn, ok := dst.(closeWriter); ok {
 		tcpConn.CloseWrite()
 	}
